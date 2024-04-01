8351f86b0273234e3163122fe1ba76af3ccdd214
pk <panuk765@gmail.com>
2024-03-31 21:54:42 -0700

    remove useEffect and useState and just use URL state directly

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -11,24 +11,18 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function InfoModal() {
-  const [isOpen, setIsOpen] = useState(false);
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
-  const shouldOpen = searchParams.has("show-info");
+  const isOpen = searchParams.has("show-info");
 
   const onOpenChange = (isOpen: boolean) => {
     const pathName = isOpen ? `${pathname}?show-info` : pathname;
     router.push(pathName);
   };
-
-  useEffect(() => {
-    setIsOpen(shouldOpen);
-  }, [shouldOpen, searchParams]);
 
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,7 +5,7 @@ export default function NavigationBar() {
     <nav className="py-4 border-b flex sticky top-0 z-50">
       <div className="container flex items-center justify-between">
         <Link href="/">Dialog Example</Link>
-        <Link className="text-blue-500 cursor-pointer hover:brightness-150" href="/?show-info">
+        <Link className="text-blue-500 cursor-pointer hover:brightness-150" href="?show-info">
           Open Dialog
         </Link>
       </div>
