f4882fff52d7022790b21a3eeb446277d644bef6
pk <panuk765@gmail.com>
2024-04-14 15:22:46 -0700

    Fix hydration mismatch error and preserve browser back-button behavior

--- src/components/modal-link.tsx
+++ src/components/modal-link.tsx
@@ -1,11 +1,11 @@
 import Link, { LinkProps } from "next/link";
 import { HTMLProps } from "react";
 
-type ModalLinkProps = React.PropsWithChildren<LinkProps & HTMLProps<HTMLAnchorElement>>;
+type ModalLinkProps = React.PropsWithChildren<Omit<LinkProps, "replace" | "scroll"> & HTMLProps<HTMLAnchorElement>>;
 
 export default function ModalLink({ children, ...rest }: ModalLinkProps) {
   return (
-    <Link scroll={false} {...rest}>
+    <Link replace={true} scroll={false} {...rest}>
       {children}
     </Link>
   );
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,26 +1,41 @@
 "use client";
 
 import { Dialog } from "@/components/ui/dialog";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type ModalProps = React.PropsWithChildren<{
   routeName: string;
 }>;
 
 export default function Modal({ children, routeName }: ModalProps) {
+  const [open, setOpen] = useState(false);
   const searchParams = useSearchParams();
   const router = useRouter();
-  const shouldOpen = searchParams.has(routeName);
+  const pathname = usePathname();
+  const isOpen = searchParams.has("show-info");
 
-  const onOpenChange = (isOpen: boolean) => {
-    if (!isOpen) {
-      //clear the query string from the url when the dialog is closed
-      router.back();
+  /**
+   * We use an effect to update the open state whenever the isOpen prop changes.
+   * This ensures that the dialog is only opened after the initial render is complete.
+   * By using an effect, we enable the ability to share the link to the dialog with others.
+   */
+  useEffect(() => {
+    setOpen(isOpen);
+  }, [isOpen]);
+
+  //whenever the open state changes, update the URL accordingly
+  function onOpenChanged(open: boolean): void {
+    if (!open) {
+      router.replace(pathname, { scroll: false });
+    } else {
+      //this ensures that the DialogTrigger button works as expected
+      router.replace(`${pathname}?show-info`, { scroll: false });
     }
-  };
+  }
 
   return (
-    <Dialog open={shouldOpen} onOpenChange={onOpenChange}>
+    <Dialog open={open} onOpenChange={onOpenChanged}>
       {children}
     </Dialog>
   );
